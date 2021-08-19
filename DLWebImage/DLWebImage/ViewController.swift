678d7488253a4f733142a14e8be3b72688b0eed8
daiqing <>
2021-08-19 17:35:23 +0800

    1.0

--- DLWebImage/DLWebImage/ViewController.swift
+++ DLWebImage/DLWebImage/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  DLWebImage
-//
-//  Created by qing on 2021/8/11.
-//
 
 import UIKit
 
@@ -16,7 +10,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell : DLTableViewCell = tableView.dequeueReusableCell(withIdentifier: "DLTableViewCell", for: indexPath) as! DLTableViewCell
-        let string = dataSource[indexPath.row]        
+        let string = dataSource[indexPath.row]
         cell.imageV.dl_webImage.url(url: string).failImage(failImage: UIImage.init(named: "liveSquare.png")).scaleType(scaleType: .scaleOriginal).state(state: .highlighted).showLoading(showLoading: true).resume()
         return cell
     }
@@ -33,6 +27,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.dataSource = self
         tableView.reloadData()
         UIButton.init().setTitle("", for: .normal)
+    }
+    
+    deinit {
+        print("deinitdeinit")
     }
     
     var gifUrl = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Ff.mgame.netease.com%2Fforum%2F201509%2F21%2F171337o26avxzpb6wpowza.gif&refer=http%3A%2F%2Ff.mgame.netease.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1631692465&t=a52a9396edae2a90eb8d697c7aedece3"
